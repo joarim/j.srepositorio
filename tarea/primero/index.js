88d7d07de66217d278a741f48b608ab6fc4e8d71
leidy sanchez <joarim3196@gmail.com>
2021-05-21 11:34:18 -0500

    subir trabajos

---
+++ tarea/primero/index.js
@@ -0,0 +1,19 @@
+let numero = 50;
+console.log("primero imprecion");
+console.log(numero);
+console.log("segundo imprecion");
+console.log(numero);
+console.log("tercero imprecion");
+console.log(numero);
+console.log("cuarto imprecion");
+console.log(numero);
+
+//pseudocodigo
+//inicio
+//escribir,numero 50
+//leer numero 50
+//imprimir cuatro veces el numero 50
+//fin
+
+
+//
